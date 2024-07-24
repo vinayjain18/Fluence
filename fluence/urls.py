661ee0e7da0e42a1047c0545a6f68cd4b7772001
Vinay Jain <48233751+vinayjain18@users.noreply.github.com>
2024-07-25 00:45:42 +0530

    Updated static_root

--- fluence/urls.py
+++ fluence/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('not-admin/', admin.site.urls),
@@ -23,3 +26,6 @@ urlpatterns = [
     path('', include('creator.urls')),
     path('auth/', include('social_django.urls', namespace='social')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
