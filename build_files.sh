635368883cd2b79f9075072c6655bcd4fdd806ae
Vinay Jain <48233751+vinayjain18@users.noreply.github.com>
2024-07-25 01:09:11 +0530

    Updated build.sh

--- build_files.sh
+++ build_files.sh
@@ -7,12 +7,12 @@ echo "Building the project..."
 python3 -m pip install -r requirements.txt
 
 # Install Node.js dependencies
-# echo "Installing Node.js dependencies..."
-# npm install
+echo "Installing Node.js dependencies..."
+npm install
 
-# # Build CSS with Tailwind
-# echo "Building CSS..."
-# npm run build-css
+# Build CSS with Tailwind
+echo "Building CSS..."
+npm run build-css
 
 echo "Make Migration..."
 python3 manage.py makemigrations --noinput
