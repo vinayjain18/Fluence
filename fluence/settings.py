344b661fa7b1f96e95136964c61ea05b8a3730f5
Vinay Jain <48233751+vinayjain18@users.noreply.github.com>
2024-07-30 22:51:41 +0530

    social django middleware added

--- fluence/settings.py
+++ fluence/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
