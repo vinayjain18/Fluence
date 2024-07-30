edb7cba05cfc5de7685ef279e22c723b2515720e
Vinay Jain <48233751+vinayjain18@users.noreply.github.com>
2024-07-30 13:29:33 +0530

    Removed unused code

--- fluence/wsgi.py
+++ fluence/wsgi.py
@@ -17,4 +17,3 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'assets'))
-app = application
