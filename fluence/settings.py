5e048a28c995d27f985b1099837f5cb033658c82
Vinay Jain <48233751+vinayjain18@users.noreply.github.com>
2024-08-04 19:35:04 +0530

    Updated

--- fluence/settings.py
+++ fluence/settings.py
@@ -119,7 +119,7 @@ WSGI_APPLICATION = 'fluence.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASS"),
